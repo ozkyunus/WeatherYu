72a55d0f0c04ed53714b74dc9470bca5d2775444
Yunus Emre Özkaya <emrezkaya4@gmail.com>
2024-07-22 15:24:12 +0300

    Initial Commit

---
+++ WeatherYu/WeatherYuApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherYuApp.swift
+//  WeatherYu
+//
+//  Created by YUNUS EMRE ÖZKAYA on 22.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherYuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
