7cd00445aa0f242fd7ba5da12bb70ca4a20ee4c4
ozkyunus <emrezkaya4@gmail.com>
2024-07-30 15:01:58 +0300

    update

--- WeatherYu/View/WelcomeView.swift
+++ WeatherYu/View/WelcomeView.swift
@@ -6,10 +6,29 @@
 //
 
 import SwiftUI
+import CoreLocationUI
 
 struct WelcomeView: View {
+    @EnvironmentObject var locationmanager: LocationManager
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            VStack(spacing: 20) {
+                Text("Welcome to the WeatherYu")
+                    .bold().font(.title)
+                Text("Please share your current location to get the weather in your area")
+                    .padding()
+            }
+            .multilineTextAlignment(.center)
+            .padding()
+            
+            LocationButton(.shareCurrentLocation) {
+                locationmanager.requestLocation()
+            }
+            .cornerRadius(30)
+            .symbolVariant(.fill)
+            .foregroundColor(.white)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
 
